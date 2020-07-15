64e21650b74a41e7fdabdcb123b77b2113513fcb
YusakuAckerman <59711650+YusakuAckerman@users.noreply.github.com>
2020-07-15 14:40:31 -0300

    111

--- chiakibot.js
+++ chiakibot.js
@@ -401,7 +401,7 @@ if (cmd.startsWith(`${prefix}setlevel`)) {
 if (cmd.startsWith(`${prefix}setbangif`)) {
     let varteste = args[0]
 
-    if (!varteste.content.includes("http")) {
+    if (!varteste.includes("http")) {
         return message.channel.send("Sim, funciona")
     }
 }
